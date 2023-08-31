3700b90b0e5d69d6229d74376c2a7f83bdd4f6d5
EthanK300 <99300748+EthanK300@users.noreply.github.com>
2023-08-31 07:38:20 -0700

    start guessing game

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+using namespace std;
+int number = 0;
+int guess = 0;
+int numGuesses = 0;
+int main(){
+  srand(time(NULL));
+  number = rand();
+  while (guess != number) {
+    cin >> guess;
+    if(guess < number){
+      cout << "You were too low! Try Again";
+      numGuesses++;
+    }else if(guess > number){
+      cout << "You were too high! Try Again";
+      numGuesses++;
+    }else{
+      cout << "You guessed something else that maybe wasn't a number...";
+    }
+  }
+  cout << "You were correct! it took " << numGuesses <<" guesses!";
+}
