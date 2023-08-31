2f55b5d8076c921efb4b34c15b3b59a0164ef2f1
U-BSD\412185 <412185@SHS-8YBF6X3.beaverton.k12.or.us>
2023-08-31 12:29:52 -0700

    Palindrome start

---
+++ palindrome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <strcmp>
+using namespace std;
+
+int main()
+{
+  char input[80];
+  char input2[80];
+  cin.get(input, 80);
+  cout << input << endl;
+  
+}
