10f035a360dde0c2933d1242c71475cc389ad738
U-BSD\412185 <412185@SHS-94W2NW3.beaverton.k12.or.us>
2023-09-11 12:23:26 -0700

    working on student list

--- StudentList/StudentList.cpp
+++ StudentList/StudentList.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
 /*
 Author: Ethan K
  */
-
-int main(){
-
-}
 
 struct Student {
   char firstName[80];
@@ -18,4 +15,60 @@ struct Student {
   float gpa = 0;
 };
 
+void ADD(vector<Student*>* StudentList);
+void PRINT(vector<Student*>* StudentList);
+void DELETE(vector<Student*>* StudentList);
+
+int main(){
+  vector<Student> StudentList;
+  char terminal[80];
+  bool active = true;
+  while(active){
+      cout << "Enter a command: " << endl;
+
+      cin.ignore();
+      cin.clear();
+      cin >> terminal;
+    
+      for(int i = 0; i < strlen(terminal) - 1; i++){
+	if(!isalpha(terminal[i])){
+	  memmove(terminal+i, terminal+1+i, strlen(terminal)-i);
+	  i--;
+	}
+      }
+      //good command string
+      if(terminal == "ADD"){
+	ADD(StudentList);
+      }else if(terminal == "PRINT"){
+	PRINT(StudentList);
+      }else if(terminal == "DELETE"){
+	DELETE(StudentList);
+      }else if(terminal == "QUIT"){
+	return 0;
+      }else{
+	cout << "Bad Input" << endl;
+      }
+    }
+  }
+}
+
+void ADD(vector<Student*>* intakeList){
+  Student* student = new Student();
+  student->firstName = firstNameIN;
+  student->lastName = lastNameIN;
+  student->studentID = studentIDIN;
+  student->gpa = gpaIN;
+}
+
+void PRINT(vector<Student*>* intakeList){
+  for(vector<Student*> iterator it = StudentList->begin(); it != StudentList->end(); ++it){
+    cout << (*it)->firstName  << ", " << (*it)->lastName << ", ID: " << (*it)->studentID << ", GPA: " << (*it)->gpa << endl;
+  }
+}
+
+void DELETE(vector<Student*>* intakeList){
+  for(vector<Student*> iterator it = StudentList->begin(); it != StudentList->end(); ++it){
+    cout << (*it)->firstName  << ", " << (*it)->lastName << ", ID: " << (*it)->studentID << ", GPA: " << (*it)->gpa << endl;
+  }
+}
 
