e5da57638983ac0d6eaf37a66529b45b9eb73a93
EthanK300 <99300748+EthanK300@users.noreply.github.com>
2023-09-25 10:16:43 -0700

    Update Movies.cpp

--- Classes/Movies.cpp
+++ Classes/Movies.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 Movies::Media() : Media(){
-
+  cout << "Enter director name: " << endl;
+  cin.clear();
+  cin >> director;
+  cin.ignore();
+  while(duration == 0){
+    cout << "Enter duration: " << endl;
+    cin.clear();
+    cin >> duration;
+    cin.ignore();
+  }
+  while(rating == 0){
+    cout << "Enter rating, can't be 0: " << endl;
+    cin.clear();
+    cin >> rating;
+    cin.ignore();
+  }
 }
